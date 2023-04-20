0437312540c1dbdc2a1a8c8de4a41ed4ac1de415
Aliimranatabey <aliimranatabey@gmail.com>
2023-04-20 13:36:37 +0300

    firsttry

---
+++ src/views/Home.vue
@@ -0,0 +1,65 @@
+<!-- <template>
+    <v-app-bar
+        color="orange"
+        elevation="5"
+    ><v-avatar
+        color="warning lighten-2"
+        size="68"
+    ></v-avatar>  
+    </v-app-bar>
+    
+</template> -->
+
+<template>
+  <v-parallax
+      height="300"
+      src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+  >
+    <v-footer
+      color="primary lighten-1"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-parallax>
+  </template>
+
+  <script>
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ],
+    }),
+    methods:{
+      clickHome:function(){
+        
+      }
+    }
+  }
+</script>
